55f99a6d2a1d3f8cae07b8675cbe1f11089cd809
denis0bej <bejan.topse.denis@gmail.com>
2024-11-29 10:50:17 +0200

    Am rescris tot codul.. mai trebuie sa fac defragmentationul

--- cdgds.s
+++ cdgds.s
@@ -1,165 +1,363 @@
 .data
-    bin: .asciz "%s\n"
-    dec: .asciz "%d\n"
-    s: .space 1024
-    fid: .long 25
-    blocksize: .long 4
-    kbsize: .long 32
+spacerstring: .asciz "--\n"
+fstring: .asciz "%d \n"
+s: .space 1024
+blocksize: .space 4
+fid: .space 1
 .text
 .global main
-
-#FITSTART
-FIT: #returneaza poz prin eax
+_print: #push element
     pushl %ebp
     movl %esp, %ebp
-    movl $0, %eax #c0
-    movl $0, %ecx
-    pushl %ebx
-    pushl %edi
-    pushl %esi
-    movl $0, %esi
-    movl 8(%ebp), %ebx   #ebx=blocksize
-    movl 12(%ebp), %edx  #edx=fid
-    movl $s, %edi  #edi=$s
-    et_FIT_loop:        #for ecx=0 ecx<1024
-        cmpl $1024, %ecx
-        je et_FIT_loop_exit
-        cmpb %dl, (%edi,%ecx,1) #POATE E ERAORE cmp 4B cu 1B
-        
-        je et_FIT_loop_ret1
+    pushl %eax
+    movl 8(%ebp), %eax
+    pushl %ecx
+    pushl %edx
 
-        cmpb $0, (%edi,%ecx,1) #if s[i]==0
-        jne et_FIT_else #daca !=0 jmp FIT_else
-        incl %eax #c0++
-        jmp et_FIT_loop_fin
-        et_FIT_else:
-            xorl %eax, %eax #c0=0
-            incl %ecx
-            movl %ecx, %esi #i0=ecx+1
-            subl $1, %ecx
-        et_FIT_loop_fin:
-        cmpl %eax, %ebx #if c0 == blocksize
-        je et_FIT_loop_ret2 #return i0
-
-        incl %ecx
-        jmp et_FIT_loop
-    et_FIT_loop_ret1:
-        movl $-1, %eax
-        jmp et_FIT_loop_exit
-    et_FIT_loop_ret2:
-        movl %esi , %eax
-        jmp et_FIT_loop_exit
-    et_FIT_loop_exit:
-
-    popl %esi
-    popl %edi
-    popl %ebx
-    popl %ebp
-    ret
-#FITEND
-
-
-
-#ADDSTART 
-ADD: # fid kbsize <r.a> 
-    pushl %ebp
-    movl %esp, %ebp
-    movl $0, %ecx
-    pushl %ebx
-    pushl %edi
-
-    movl 8(%ebp), %eax   #eax=kbsize
-    movl 12(%ebp), %ebx  #ebx=fid
-    movl $8, %edi  #edi=$8
-    xorl %edx, %edx #edx=0
-
-    div %edi #eax = eax(kbsize)/edi(8)
-    cmp $0, %edx
-    je et_ADD_if1
-    inc %eax
-    et_ADD_if1:
-    mov %eax, %edi  #edi=blocksize
-
-    pushl %ebx #fid
-    pushl %edi #blocksize
-    call FIT   #rez in %eax
+    push %eax
+    movl $fstring, %edx
+    pushl %edx
+    call printf
     addl $8, %esp
-    
-    cmpl $-1, %eax #verif daca poz e valida
-    je et_ADD_exit
-    #for i=poz(%eax); i<poz+blocksize(eax+edi) i++
-    movl %eax, %ecx 
-    movl %eax, %edx
-    addl %edi, %edx #edx = poz + blocksize
-    mov $s, %edi #edi = adresa vector
-    et_ADD_loop:
-        cmp %edx, %ecx #i<poz+blocksize(eax+edi)
-        je et_ADD_loop_exit
-        mov %ebx, (%edi, %ecx, 1) #s[i]=fID;
-        inc %ecx    #i++
-        jmp et_ADD_loop
-        et_ADD_loop_exit:
-    et_ADD_exit:
-    popl %ebx
-    popl %edi
+    popl %edx
+    popl %ecx
+    popl %eax
     popl %ebp
     ret
-#ADDEND
-
-
-
-main:   
-
-    movl $0, %ecx
+_spacer:
+    pushl %eax
+    pushl %ecx
+    pushl %edx
+    pushl $spacerstring
+    call printf
+    addl $4, %esp
+    popl %edx
+    popl %ecx
+    popl %eax
+    ret
+PRINT:  #push size
+    pushl %ebp
+    movl %esp, %ebp
+    pushl %eax
+    pushl %ecx
+    pushl %edx
+    pushl %edi
+    movl 8(%ebp), %eax
     movl $s, %edi
-    et_init:
-        cmpl $1025, %ecx
-        je et_init_exit
-        movb $0,(%edi, %ecx, 1)
-        incl %ecx
-        jmp et_init
-        et_init_exit:
-
-
-    push fid
-    push kbsize
-    call ADD
-    add $8, %esp
-
-    movl $0, %ecx
-    movl $s, %edi
+    xorl %ecx, %ecx
     et_print_loop:
-        cmpl $8, %ecx
-        je et_print_loop_exit
-
-        pushl %ecx
-
-        pushl (%edi, %ecx, 1)
-        pushl $dec
-        call printf
-        addl $8, %esp
-
-        popl %ecx
-
+        cmp %eax, %ecx
+        je et_print_loop_exit 
+        xorl %edx, %edx
+        movb (%edi, %ecx, 1),%dl
+        push %edx
+        call _print
+        addl $4, %esp
         incl %ecx
         jmp et_print_loop
         et_print_loop_exit:
+    popl %edi
+    popl %edx
+    popl %ecx
+    popl %eax
+    popl %ebp
+    ret
+FIT: #push blocksize; push fid
+    pushl %ebp
+    movl %esp, %ebp
+    pushl %ebx
+    pushl %ecx
+    pushl %edi
+    pushl %edx
+    movl $s, %edi #edi = vector(s)
+    movl 8(%ebp), %ecx
+    movb %cl, fid
+    movl 12(%ebp), %ecx
+    movl %ecx, blocksize
+    xorl %ecx, %ecx #i=0
+    xorl %ebx, %ebx #c0=0
+    xorl %eax, %eax #i0=0
+    #INIT ^^^
+    
+    FIT_loop:
+            
+        cmp $1024, %ecx
+        je FIT_loop_exit
+        xorl %edx, %edx
+        movb (%edi, %ecx, 1), %dl #dl=s[i]
 
+        cmpb fid, %dl   #cmp fid cu s[i]
+        je FIT_return_m1    #fid == s[i]
+        
+        cmpb $0, %dl    #cmp 0 cu s[i]
+        jne FIT_else
+        incl %ebx       #s[i]==0 >> ebx++ (c0++)
+        jmp FIT_else_exit
+        FIT_else:       #s[i]!=0
+            xorl %ebx, %ebx #c0=0
+            incl %ecx
+            movl %ecx, %eax #i0=i+1
+            subl $1, %ecx
+            FIT_else_exit:
+        inc %ecx
+        jmp FIT_loop
+
+        cmp blocksize, %ebx
+        je FIT_return_i0
+
+        FIT_return_m1:
+            movl $-1, %eax
+            jmp FIT_loop_exit
+        FIT_return_i0:
+            jmp FIT_loop_exit
+        FIT_loop_exit:
+    popl %edx
+    popl %edi
+    popl %ecx
+    popl %ebx
+    popl %ebp
+    ret
+ADD: #push kbsize; push fid
+    pushl %ebp
+    movl %esp, %ebp
+    pushl %edi
+    pushl %ecx
+    pushl %eax
+    pushl %edx
+    movl 12(%ebp), %eax # eax = kbsize
+    movl 8(%ebp), %edi
+    xorl %edx, %edx
+    movl $8, %ecx
+    divl %ecx #eax = blocksize(kbsize/8)
+
+    cmpl $0, %edx   #daca kbsize nu e mult de 8
+    je ADD_rotund_end
+    incl %eax       #ceil %eax
+    ADD_rotund_end:
+
+    cmp $2, %eax #eax < 1
+    jnl ADD_min_2
+    mov $2, %eax
+    ADD_min_2:
+
+    movl %eax, blocksize
+    pushl blocksize 
+    pushl %edi
+    call FIT
+    addl $8, %esp
+    cmp $-1, %eax #cmp poz cu -1
+    je ADD_exit
+
+    movl %eax, %ecx #i=startpos
+    addl blocksize, %eax #end
+    movl 8(%ebp), %edx #edx = fid
+    movl $s, %edi
+    #ecx=i0(start) eax=i0+blocksize(end) edi=$s dl=fid
+    ADD_loop:
+
+        cmp %eax, %ecx
+        je ADD_exit
+        movb %dl, (%edi, %ecx, 1)
+        inc %ecx
+        jmp ADD_loop
+
+    ADD_exit:
+    popl %edx
+    popl %eax
+    popl %ecx
+    popl %edi
+    popl %ebp
+    ret
+GET:#push fid
+    pushl %ebp
+    movl %esp, %ebp
+    movl $-1, %eax
+    movl $-1, %edx
+    pushl %ebx
+    movl 8(%ebp), %ebx  #ebx = fid
+    pushl %edi
+    movl $s, %edi       #edi = $s
+    pushl %ecx
+    xorl %ecx, %ecx     #ecx = 0
+
+    GET_loop:
+        cmpl $1024, %ecx
+        je GET_loop_exit
+        
+        cmpl $-1, %eax
+        jne GET_cmp1_end
+        cmpb (%edi, %ecx, 1), %bl #cmp v[i] cu fid
+        jne GET_cmp1_end
+        movl %ecx, %eax
+        GET_cmp1_end:
+
+        cmpb (%edi, %ecx, 1), %bl #cmp v[i] cu fid
+        jne GET_cmp2_end
+        movl %ecx, %edx
+        GET_cmp2_end:
+
+        cmpb (%edi, %ecx, 1), %bl #cmp v[i] cu fid
+
+
+
+        incl %ecx
+        jmp GET_loop
+        GET_loop_exit:
+
+    cmp $-1, %eax
+    jne GET_exit
+    movl $0, %eax
+    movl $0, %edx
+
+    GET_exit:
+    popl %ecx
+    popl %edi
+    popl %ebx
+    popl %ebp
+    ret
+DELETE:# push fid
+    
+    pushl %ebp
+    movl %esp, %ebp
+    pushl %ecx
+    pushl %edi
+    pushl %ebx
+    movl $s, %edi
+    movl 8(%ebp), %ebx
+
+    pushl %ebx
+    call GET        #[eax edx]
+    add $4, %esp
+
+    movl %eax, %ecx
+    DELETE_loop:
+        cmpl %edx, %ecx
+        jg DELETE_loop_exit
+
+        movb $0, (%edi, %ecx, 1)
+
+        incl %ecx
+        jmp DELETE_loop
+        DELETE_loop_exit:
+
+    popl %ebx
+    popl %edi
+    popl %ecx
+    popl %ebp
+    ret
+DEFRAGMENTATION:
+    pushl %ebp
+    movl %esp, %ebp
+    pushl %eax
+    pushl %ebx
+    pushl %ecx
+    pushl %edx
+    pushl %edi
+    pushl %esi
+
+    movl $s, %edi
+    xorl %esi, %esi # esi = false
+    DEFRAGMENTATION_while:
+        cmpl $0, %esi
+        jne DEFRAGMENTATION_while_end
+
+        xorl %ecx, %ecx
+        DEFRAGMENTATION_for1:
+            cmpl $1022, %ecx
+            je DEFRAGMENTATION_for1_end
+
+            xorl %edx, %edx
+            movb (%edi, %ecx, 1), %dl #s[i]
+            incl %ecx
+            movb (%edi, %ecx, 1), %dh #s[i+1]
+            subl $1, %ecx
+
+            movl $1, %esi #esi = true
+            DEFRAGMENTATION_if:
+                xorl %ebx, %ebx
+                movb $2, %bh
+                movb $3, %bl
+                cmpb $0, %dl #cmp s[i] cu 0
+                jne DEFRAGMENTATION_arg1_exit
+                movb $1, %bl
+                DEFRAGMENTATION_arg1_exit:
+                cmpb $0, %dh #cmp s[i+1] cu 0
+                je DEFRAGMENTATION_arg2_exit
+                movb $1, %bh
+                DEFRAGMENTATION_arg2_exit:
+                cmpb %bh, %bl
+                jne DEFRAGMENTATION_if_end
+                
+                movl $0, %esi #esi = false
+                movl %ecx, %ebx
+                DEFRAGMENTATION_for2:
+                    cmpl $1022, %ebx
+                    je DEFRAGMENTATION_for2_end
+                    
+                    xorl %edx, %edx
+                    incl %ebx
+                    movl $s, %edi
+                    movb (%edi, %ebx, 1), %dh #s[j+1]
+                    subl $1, %ebx
+
+                    movb %dh, (%edi, %ebx, 1) #s[j]=s[j+1]
+
+                    incl %ebx
+                    jmp DEFRAGMENTATION_for2
+                DEFRAGMENTATION_for2_end:
+
+                sub $2, %ecx
+                DEFRAGMENTATION_if_end:
+
+            incl %ecx
+            jmp DEFRAGMENTATION_for1
+            DEFRAGMENTATION_for1_end:
+
+        jmp DEFRAGMENTATION_while
+        DEFRAGMENTATION_while_end:
+
+    popl %esi
+    popl %edi
+    popl %edx
+    popl %ecx
+    popl %ebx
+    popl %eax
+    popl %ebp
+    ret
+
+main:
+    pushl $64
+    pushl $25
+    call ADD
+    addl $8, %esp
+
+    pushl $16
+    pushl $211
+    call ADD
+    addl $8, %esp
+
+
+
+    pushl $16
+    call PRINT
+    addl $4, %esp
+
+    call _spacer
+
+    pushl $25
+    call DELETE
+    addl $4, %esp
+    
+    pushl $1
+    pushl $2
+    call ADD
+    addl $8, %esp
+
+
+    pushl $16
+    call PRINT
+    addl $4, %esp
 et_exit:
-    movl $1, %eax           
-    xorl %ebx, %ebx           
-    int $0x80                  
-
-#########
-push %eax
-push %ecx
-push %edx
-
-push %edx
-push $dec
-call printf
-add $8, %esp
-
-popl %eax
-popl %ecx
-popl %edx
+movl $1, %eax
+xorl %ebx, %ebx
+int $0x80
